70b1b8c4a0ce387bf7f390fddc6845bd293d14ae
Nirmal <nirmalthanga@gmail.com>
2026-08-03 09:55:41 +0530

    js vs ts data

---
+++ typeScript/dataTypes.ts
@@ -0,0 +1,25 @@
+// JavaScript is dynamically typed language, which means we don't have to specify the data type of a variable when we declare it. The data type of a variable is determined at runtime based on the value assigned to it. However, this can lead to unexpected behavior and bugs in our code, especially in large codebases.
+
+// TypeScript is a statically typed language, which means we can specify the data type of a variable when we declare it. This allows us to catch errors at compile time, before our code is executed. TypeScript provides several built-in data types, as well as the ability to create custom data types using interfaces and classes.
+
+// In this file, we will explore the different data types available in TypeScript and how to use them effectively in our code. 
+
+/* 
+
+// JavaScript Example
+let x = 10 // number
+x = "Hello" // string
+x = true // boolean
+
+// TypeScript Example
+let y = 10 // number
+y = "Hello" // Error: Type 'string' is not assignable to type 'number'
+y = true // Error: Type 'boolean' is not assignable to type 'number'
+
+// In the above example, we can see that in JavaScript, we can assign different data types to the same variable, which can lead to unexpected behavior. In TypeScript, we can specify the data type of a variable, which helps us catch errors at compile time and write more robust code.      
+
+*/
+
+// To run JS file - node <file_name>.js - need to add folder path if any - node javaScript/dataTypes.js
+// To run TS file - tsc <file_name>.ts - need to add folder path if any - tsc typeScript/dataTypes.ts
+
